36b9a9d68ba244cb7f02c040d37e3fc282a16c72
Mdasif7324 <148074996+Mdasif7324@users.noreply.github.com>
2024-10-03 07:35:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+// let a=3;
+// let b=5;
+// let c=10;
+// let d =a+b+c;
+// console.log(d);
+// let h ="There is a huge demand for people skilled in every area of e-commerce, especially web development";
+// console.log(h);
+// document.writeln(h);
